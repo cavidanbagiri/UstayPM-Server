c2e6e8b06e736b86f0a0c328956b5bba370a666c
cavidanbagiri <cavidanbagiri@gmail.com>
2023-10-29 20:15:58 +0300

    Change Connection String

--- models/index.js
+++ models/index.js
@@ -15,8 +15,23 @@ if (config.use_env_variable) {
 } else {
   // sequelize = new Sequelize(config.database, config.username, config.password, config);
   // let connection_string = `postgres://${process.env.DB_PRODUCTION_USER}:${process.env.DB_PRODUCTION_PASSWORD}@${process.env.DB_PRODUCTION_HOST}/${process.env.DB_PRODUCTION_NAME}?sslmode=no-verify`;
-  let connection_string = `postgres://${process.env.DB_PRODUCTION_USER}:${process.env.DB_PRODUCTION_PASSWORD}@${process.env.DB_PRODUCTION_HOST}/${process.env.DB_PRODUCTION_NAME}?sslmode=prefer`; 
-  sequelize = new Sequelize(connection_string);
+  // let connection_string = `postgres://${process.env.DB_PRODUCTION_USER}:${process.env.DB_PRODUCTION_PASSWORD}@${process.env.DB_PRODUCTION_HOST}/${process.env.DB_PRODUCTION_NAME}?ssl=true`; 
+  // sequelize = new Sequelize(connection_string);
+  sequelize = new Sequelize('ustay_pm', 'ustay_pm_user', 'w6YSQ33DBLKS9YPaIMj5PiDjKB13QDPi', {
+    host: 'dpg-ck8u83vsasqs739c6oh0-a.frankfurt-postgres.render.com',
+    dialect: 'postgres',
+    dialectOptions: {
+      
+      // Your pg options here
+      ssl: true
+      //{
+        // sslmode: 'prefer', 
+        // require: true,
+        // rejectUnauthorized: true,
+        // ca: fs.readFileSync("C:\\Users\\Ramzes\\Downloads\\BaltimoreCyberTrustRoot.crt.pem")
+      // }
+    }
+  });
 }
 
 fs
