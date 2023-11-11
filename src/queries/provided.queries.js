c4cbc57235bde9cb60ebb3c05e16628c43c6cc07
cavidanbagiri <cavidanbagiri@gmail.com>
2023-11-11 23:48:46 +0300

    Optimize Provide Data Queries

--- src/queries/provided.queries.js
+++ src/queries/provided.queries.js
@@ -2,7 +2,7 @@ class ProvidedQueries {
   
   // Fetc Provided Data
   static fetch_provide_data = `
-    select sm_models.sm_num, sm_models.sm_material_name, provided_models.provided_amount, sm_models.sm_material_unit, 
+    select sm_models.sm_num, sm_models.sm_material_name as material_name, provided_models.provided_amount as amount, sm_models.sm_material_unit as unit, 
     provided_models.provided_date, provided_models.serial_no, provided_models.unique_id, provided_models.deliver_to, provided_models.card_number
     from provided_models
     left join warehouse_models on provided_models."warehouseId" = warehouse_models.id
