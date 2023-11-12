10fc6228165da7f2f6162764150a752027cdf58b
cavidanbagiri <cavidanbagiri@gmail.com>
2023-11-12 14:00:57 +0300

    Fix Filter QUeries

--- src/queries/common_queries.js
+++ src/queries/common_queries.js
@@ -3,7 +3,7 @@ class CommonQueries {
   static select_all_stf_query = `
   select stf_models.id as stf_id, stf_models.stf_num, stf_models.completed, stf_models.material_type, stf_models.material_name, stf_models.material_amount as amount,
   stf_models.material_unit as unit, stf_models."createdAt", stf_models."projectId" as project_id, stf_models."departmentId" as department_id,
-  concat(users_models.name , ' ', users_models.surname)  as username, fields_models.field_name
+  INITCAP(concat(users_models.name , ' ', users_models.surname))  as username, fields_models.field_name
   from stf_models
   left join users_models on users_models.id = stf_models."userId"
   left join fields_models on fields_models.id = "fieldId"
@@ -14,8 +14,8 @@ class CommonQueries {
   select sm_models.id as sm_id, sm_models."stfId" as stf_id, sm_models.sm_num, stf_models.stf_num, situation_models.status_name as situation, sm_models.sm_material_name,
   sm_models.sm_material_amount, sm_models.sm_material_unit, sm_models.price, sm_models.total, sm_models.currency, sm_models.left_over, sm_models.approximate_date,
   sm_models."createdAt", sm_models."projectId" as project_id, sm_models."departmentId" as department_id,
-  concat(users_models.name , ' ', users_models.surname)  as orderer,
-  concat(um.name, ' ', um.surname ) as supplier,
+  INITCAP(concat(users_models.name , ' ', users_models.surname) ) as orderer,
+  INITCAP(concat(um.name, ' ', um.surname )) as supplier,
   vendors_models.vendor_name
   from sm_models
   left join stf_models on sm_models."stfId"=stf_models.id
@@ -32,8 +32,8 @@ class CommonQueries {
   warehouse_models.delivery_material_amount as amount, warehouse_models.stock, warehouse_models.delivery_material_unit as unit,
   sm_models.price, sm_models.total, sm_models.currency, sm_models.left_over, 
   warehouse_models."createdAt", sm_models."projectId" as project_id, sm_models."departmentId" as department_id,
-  concat(users_models.name , ' ', users_models.surname)  as orderer,
-  concat(um.name, ' ', um.surname ) as supplier,
+  INITCAP(concat(users_models.name , ' ', users_models.surname))  as orderer,
+  INITCAP(concat(um.name, ' ', um.surname )) as supplier,
   vendors_models.vendor_name
   from warehouse_models
   left join sm_models on warehouse_models."smId" = sm_models.id 
@@ -65,8 +65,17 @@ class CommonQueries {
   `;
 
   // Fetch All Procurement Users
+  // static select_stf_created_users_names = `
+  // select id, INITCAP(concat(name, ' ', surname)) as ordered_name from users_models
+  // `;
   static select_stf_created_users_names = `
-  select id, Initcap(concat(name, ' ', surname)) as ordered_name from users_models
+    select users_models.id,  count(distinct(stf_num)) as stf_data, 
+    INITCAP(concat(users_models.name, ' ', users_models.surname )) as ordered_name
+    from stf_models
+    left join users_models on users_models.id = stf_models."userId"
+    where
+    stf_models."userId" in ( select distinct "userId" from stf_models )
+    group by "userId", users_models.name, users_models.surname, users_models.id
   `;
   
   // Fetch Department
@@ -78,7 +87,7 @@ class CommonQueries {
   static fetch_stf_row_inform = `
   SELECT stf_models.id as stf_id, stf_models.stf_num, stf_models.material_type, stf_models.material_name, stf_models.material_amount,
   stf_models.material_unit, stf_models.material_link, stf_models.material_comment, stf_models.completed, stf_models."createdAt",
-  CONCAT(users_models.name, ' ', users_models.surname) as Ordered_by, 
+  INITCAP(CONCAT(users_models.name, ' ', users_models.surname)) as Ordered_by, 
   department_models.department_name,
   fields_models.field_name,
   sm_models.sm_num, sm_models.sm_material_name, sm_models.sm_material_amount, sm_models.sm_material_unit, sm_models.price,
@@ -88,8 +97,8 @@ class CommonQueries {
   warehouse_models.delivery_material_price, warehouse_models.delivery_material_total, warehouse_models.delivery_material_currency,
   warehouse_models.doc_number, warehouse_models.doc_date, warehouse_models.certificate, warehouse_models.passport, warehouse_models.stock,
   warehouse_models.doc_number, warehouse_models.doc_date, warehouse_models.certificate, warehouse_models.passport, warehouse_models.stock, 
-  CONCAT(us_mod.name, ' ', us_mod.surname) as Supplier_name,
-  CONCAT(u_m.name, ' ',u_m.surname) as Accepted_by
+  INITCAP(CONCAT(us_mod.name, ' ', us_mod.surname)) as Supplier_name,
+  INITCAP(CONCAT(u_m.name, ' ',u_m.surname)) as Accepted_by
   FROM stf_models 
   LEFT JOIN users_models ON  stf_models."userId" = users_models.id
   LEFT JOIN department_models ON stf_models."departmentId"=department_models.id
