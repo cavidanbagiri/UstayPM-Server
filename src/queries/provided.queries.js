5b7e85b7a14186eb8b4c47639c8b575c97a7e36f
cavidanbagiri <cavidanbagiri@gmail.com>
2023-12-14 23:40:54 +0300

    fetch provide materials with stf_id for getting stfrowinform data

--- src/queries/provided.queries.js
+++ src/queries/provided.queries.js
@@ -2,11 +2,12 @@ class ProvidedQueries {
   
   // Fetc Provided Data
   static fetch_provide_data = `
-    select sm_models.sm_num, sm_models.sm_material_name as material_name, provided_models.provided_amount as amount, sm_models.sm_material_unit as unit, 
+    select sm_models.sm_num, stf_models.id as stf_id, sm_models.sm_material_name as material_name, provided_models.provided_amount as amount, sm_models.sm_material_unit as unit, 
     provided_models.provided_date, provided_models.serial_no, provided_models.unique_id, provided_models.deliver_to, provided_models.card_number
     from provided_models
     left join warehouse_models on provided_models."warehouseId" = warehouse_models.id
     left join sm_models on warehouse_models."smId" = sm_models.id
+    left join stf_models on sm_models."stfId" = stf_models.id
   `;
 }
 
